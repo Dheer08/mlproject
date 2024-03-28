56742e7254e7b74d6b699287681b6b981937339b
Rakshith Dheer <rakshithdheerreddy08@gmail.com>
2024-03-28 08:26:49 +0530

    EDA in notebook

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 '''
 Any exceptions to be controlled, the sys module has that information
@@ -23,13 +23,12 @@ class CustomException(Exception):
         return self.error_message
     
 
-'''
 if __name__=="__main__":
     try:
         a = 1/0
     except Exception as e:
         logging.info("Divide by Zero")
         raise CustomException(e,sys)
-'''
+
 
 
